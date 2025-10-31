fb76d442a0bd6c00fa035e65def7b577f02d428f
WillR224 <will.rose.224@gmail.com>
2025-10-31 10:51:38 +0000

    Edited mpags-cipher to test c++ scripts

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -6,4 +6,15 @@ int main()
     multiline comment*/
 
     std::cout << "Hello, World!\n";
+
+    int a; //Not initalised, takes the value in the memory 
+    a = 12;
+    int b = 1; 
+    int c(13); 
+    double d{12.593}; // c++11 Uniform initialization. 
+    
+    int e = c*d;
+    std::cout << a << "\n";
+    std::cout << e << "\n";
+
 }
